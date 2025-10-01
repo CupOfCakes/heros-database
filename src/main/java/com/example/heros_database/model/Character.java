18e237379b161a432eba3647c2e77ef6f1d7c267
CupOfCakes <heitor.scarv@gmail.com>
2025-10-01 00:56:18 -0300

    make character class

---
+++ src/main/java/com/example/heros_database/model/Character.java
@@ -0,0 +1,58 @@
+package com.example.heros_database.model;
+
+import java.util.Arrays;
+
+public class Character {
+    public int id;
+    public String name;
+    public String image;
+    public String description;
+    public String[] tags;
+    public String lastUpdate;
+
+
+    private String neutralize;
+    private String threatLevel;
+    private String observationLevel;
+
+    // Construtor vazio necessário para JSON
+    public Character() {}
+
+    // Getters e setters
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getImage() { return image; }
+    public void setImage(String image) { this.image = image; }
+
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+
+    public String[] getTags() { return tags; }
+    public void setTags(String[] tags) { this.tags = tags; }
+
+    public String getLastUpdate() { return lastUpdate; }
+    public void setLastUpdate(String lastUpdate) { this.lastUpdate = lastUpdate; }
+
+    public String getNeutralize() { return neutralize; }
+    public void setNeutralize(String neutralize) { this.neutralize = neutralize; }
+
+    public String getThreatLevel() { return threatLevel; }
+    public void setThreatLevel(String threatLevel) { this.threatLevel = threatLevel; }
+
+    public String getObservationLevel() { return observationLevel; }
+    public void setObservationLevel(String observationLevel) { this.observationLevel = observationLevel; }
+
+    @Override
+    public String toString() {
+        return "Character{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", tags=" + Arrays.toString(tags) +
+                '}';
+    }
+
+}
--- src/main/java/com/example/heros_database/records_screen.java
+++ src/main/java/com/example/heros_database/records_screen.java
@@ -1,9 +1,16 @@
 package com.example.heros_database;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.IOException;
 
 public class records_screen extends Application {
 
@@ -11,7 +18,24 @@ public class records_screen extends Application {
     public void start(Stage primaryStage){
         BorderPane root = new BorderPane();
 
+        //search
         VBox vbox = new VBox(10);
+
+        vbox.setAlignment(Pos.CENTER);
+        vbox.setStyle("-fx-background-color: rgba(0,0,0,0.6); -fx-padding: 20; -fx-background-radius: 10;");
+
+        TextField searchField = new TextField();
+        searchField.setPromptText("Search");
+
+        vbox.getChildren().add(searchField);
+        vbox.setAlignment(Pos.CENTER);
+
+        root.setTop(vbox);
+
+        //names
+
+        String path = "/jsons/users.json";
+
 
 
 
